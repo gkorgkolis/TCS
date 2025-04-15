598d5520f00bb76c201a6a847222e169e8049f2a
Nikolas Kougioulis <nkougioulis95@gmail.com>
2025-04-16 00:13:38 +0300

    Update tempogen/temporal_scm.py

--- code/tempogen/temporal_scm.py
+++ code/tempogen/temporal_scm.py
@@ -2,7 +2,6 @@ import sys
 
 import numpy as np
 import pandas as pd
-# import networkx as nx
 from tqdm import trange
 
 from tempogen.temporal_causal_structure import TempCausalStructure
@@ -40,30 +39,28 @@ class TempSCM:
             initial_graph=None,             # ...
             funcs=None,                     # specific to node parameterizarion
             z_distributions=None,           # ...
-            z_types=None,                   # ...
-            warmup_steps=20                 # warmup steps to discard during ancestral sampling
+            z_types=None                    # ...
     ) -> None:
         """
         Initializes a TempSCM object. 
 
-        Args
-        ----
-        causal_structure (TempCausalStructure) : overrules the random graph generation process and explicitly defines 
-                            the structure; specific to TempCausalStructure;
-        method (str) : specifies the method that creates the random graph; supported methods are *C* for a custom Erdos-Renyi 
-                    like approach, *ER* for an approach based on the Erdos-Renyi implementation of NetworkX, and *BA* for an approach 
-                    based on the Barabasi-Albert implementation of NetworkX.
-        n_vars (int) : the number of variables for the random generation of the structure
-        n_lags (int) : the number of lags for the random generation of the structure
-        node_names (list) : the names of the nodes for the causal structure; 
-                                  if not provided, it follows an alphabetic order (capped at 26)
-        p_edge (float) : the global probability for edge creation; specific to *C* and *ER* methods
-        m (int) : the average number of edges to be added; specific to the *BA* method
-        seed (int) : the seed for the internal random generators; specific to the *BA* method
-        initial_graph (nx.DiGraph) : the starting point of the random generation; specific to the *BA* method
-        funcs (any) : the functional dependencies; specific to node parameterizarion
-        z_distributions (any) : the noise distribution; specific to node parameterizarion
-        z_types (any) : the noise type; specific to node parameterizarion
+        Args:
+            - causal_structure (pd.DataFrame) : overrules the random graph generation process and explicitly defines the structure; 
+                                specific to TempCausalStructure.
+            - method (str) : specifies the method that creates the random graph; supported methods are `C` for a custom Erdos-Renyi like 
+                    approach, `ER` for an approach based on the Erdos-Renyi implementation of NetworkX, and `BA` for an approach 
+                    based on the Barabasi-Albert implementation of NetworkX. Default is `C`
+            - n_vars (int) : the number of variables for the random generation of the structure. Default is `5`.
+            - n_lags (int) : the number of lags for the random generation of the structure. Default is `2`
+            - node_names (list) : the names of the nodes for the causal structure; 
+                                  if not provided, it follows an alphabetic order (capped at `26`)
+            - p_edge (float) : the global probability for edge creation; specific to `C` and `ER` methods
+            - m (int) : the average number of edges to be added; specific to the `BA` method. Default is `3`
+            - seed (int) : the seed for the internal random generators; specific to the `BA` method
+            - initial_graph (nx.DiGraph) : the starting point of the random generation; specific to the `BA` method
+            - funcs (any) : Functions for the parameterization of the nodes. Default is `None`.
+            - z_distributions (any) : Functions for the parameterization of the noise. Default is `None`.
+            - z_types (any) : ..
         """
         # set-up the causal structure of the SCM
         if causal_structure is None:
@@ -107,133 +104,73 @@ class TempSCM:
         # initialize the time-series, as a pd.DataFrame
         self._reset_time_series()
 
-        self.warmup_steps = warmup_steps
 
-
-    def forward(self, with_effect_size=False, clipping=False, interventional=None, intervention_type=None, verbose=False):
+    def forward(self, with_effect_size=False, clipping=True, verbose=False):
         """
-        Performs ancestral sampling on the temporal SCM for one time-step, and outputs the variable value for the next one.
+        Performs ancestral sampling on the temporal SCM for one time-step, and outputs the variable value for next one.
         As a method, it takes no positional arguments and returns no output, but it internally updates the time-series attribute 
         and the archive of each node. The method may get the optional argument with_effect_size. If true, it multiplies each parent 
         value with its effect size.
-    
+
         Args
         ---- 
-        with_effect_size (bool) : default to false. If true, multiplies each fetched parent values with 
-              its corresponding effect size 
-        clipping (bool) : if true, clips the values to a specific range
-        interventional (dict) : dictionary of the form {scm.node_name - value} specifying the intervention values for the nodes.
-                If int or float, performs the specified intervention for all time-steps. 
-                If numpy.ndarray of length n_samples, performs the specified interventions for each time-step. An numpy.nan value
-                denotes that no intervention is taking place.  
-                If *int* or *float*, performs the specified intervention for all time-steps. 
-                If *numpy.ndarray*, then performs the specified interventions for each time-step. 
-                A *numpy.nan* value denotes that no intervention is taking place.  
-        intervention_type (str) : 'hard' or 'soft'. For hard interventions, the intervention value is being explicitely set. For
-              soft interventions, the intervention value is added to the existing time-series value at each timestep. 
-        verbose (bool) : if true, prints the progress of the generation, including intervention details
-        Examples:
-        --------
-            >>> scm.generate_time_series(n_samples=5, verbose=True, clipping=False, 
-                                        interventional={'A': 1.0, 'B': np.array([1.0, 2.0, 3.0, np.nan, np.nan])}, intervention_type='hard')
+            - with_effect_size (bool) : Default is `False`. If `True`, multiplies each fetched parent values with 
+                        its corresponding effect size 
+            - clipping (bool) : if `True`, clips the values to a specific range. Default is `True` 
+            - verbose (bool) : if `True`, print the progress of the generation. Default is `False`.
         """
-        # Get current time-step
-        current_time_step = len(self.time_series)
-        
-        # Calculate the variable values for the next time-step
+        # get current time-step
+        curren_time_step = len(self.time_series)
+        # calculate the variable values for the next time-step 
         for temp_node in self.temp_nodes_sorted:
-            # Get parent values
-            if len(temp_node.pa) == 0:
+            # get parent values
+            if len(temp_node.pa)==0:
                 parent_values = []
             else:
                 parent_values = [
                     self.time_series.loc[
-                        current_time_step - int(temp_node.pa.loc[idx, 'lag']), 
+                        curren_time_step - int(temp_node.pa.loc[idx, 'lag']), 
                         temp_node.pa.loc[idx, 'parent']
                     ] for idx in temp_node.pa.index
                 ]
-    
             if with_effect_size:
                 parent_values = list(parent_values * temp_node.pa["effect"])
-    
             res = temp_node.forward(parent_values=parent_values).numpy()
-    
-            # apply interventions
-            if interventional is not None and temp_node.name in interventional:
-                intervention_value = interventional[temp_node.name]
-                intervention_applied = False 
-    
-                if intervention_type == 'soft':
-                    if isinstance(intervention_value, np.ndarray):
-                        if not np.isnan(intervention_value[current_time_step - self.warmup_steps - self.causal_structure.n_lags]):
-                            res += intervention_value[current_time_step - self.warmup_steps - self.causal_structure.n_lags]
-                            intervention_applied = True
-                    elif isinstance(intervention_value, (int, float)):
-                        if not np.isnan(intervention_value):
-                            res += intervention_value
-                            intervention_applied = True
-                elif intervention_type == 'hard':
-                    if isinstance(intervention_value, np.ndarray):
-                        if not np.isnan(intervention_value[current_time_step - self.warmup_steps - self.causal_structure.n_lags]):
-                            res = intervention_value[current_time_step - self.warmup_steps - self.causal_structure.n_lags]
-                            intervention_applied = True
-                    elif isinstance(intervention_value, (int, float)):
-                        if not np.isnan(intervention_value):
-                            res = intervention_value
-                            intervention_applied = True
-                else:
-                    raise ValueError("Interventions may only take values 'soft' or 'hard'.")
-                
-                if verbose:
-                    if intervention_applied:
-                        print(f'Time step: {current_time_step}, Node: {temp_node.name}, '
-                              f'Intervention type {intervention_type}, Applied Value: {res}')
-                    else:
-                        print(f'Time step: {current_time_step}, Node: {temp_node.name}, '
-                              f'No intervention applied (NaN encountered)')
-    
-            # Clip values to avoid extreme numbers
+            # CLIP values to avoid infimums and supremums
+            # TODO: Check for non-stationarity (e.g. Dickey-Fuller test) and convert to stationary using MA finite differences? 
             if clipping:
-                res = np.clip(res, a_min=-15000, a_max=15000)
+                res = np.clip(res, a_min=-50, a_max=50)
+
+            self.time_series.loc[curren_time_step, temp_node.name] = res
+            # debugging to get the hang on what's happening with nans and infimums
+            if verbose:
+                if temp_node.name == 'A' and curren_time_step % 10 == 0:
+                    print(f'Time step: {curren_time_step}, Node: {temp_node.name}, Value: {res}')
+                    print('\n')
     
-            # Update the time-series with the node's value before processing children
-            self.time_series.loc[current_time_step, temp_node.name] = res
-    
-            # Verbose logging for normal node updates
-            if verbose and interventional is None:
-                print(f'Time step: {current_time_step}, Node: {temp_node.name}, Value: {res}')
-            elif verbose and interventional is not None and not intervention_applied:
-                # Logging for when no intervention was applied
-                print(f'Time step: {current_time_step}, Node: {temp_node.name}, Value (No Intervention): {res}')
-    
-        
+
     def generate_time_series(self, n_samples, warmup_steps=20, with_effect_size=False, 
-                            clipping=False, intervention_type=None, interventional=None, verbose=False) -> pd.DataFrame:
+                            clipping=True, verbose=True) -> pd.DataFrame:
         """
         Generate and output a time-series dataset of *n_samples*, through ancestral sampling on the temporal SCM. 
 
         Args
         ----
-        n_samples (int) : the length of the generated dataset  
-        warmup_steps (int) : number of excess forward steps to perform at start, and then discard them; this is done as 
-                    the initialization is peformed through pure noise, which is not representative of the time-series and degraded the 
-                    quality of the casual; TODO - determine through plots, statistical measures etc a reasonable default warmup-step
-                    with_effect_size (bool) : default to false. If true, multiplies each fetched parent values with 
-                    its corresponding effect size 
-        clipping (bool) : if true, clips the values to a specific range
-        intervention_type (str) : the type of intervention to be performed; Soft or Hard.
-        interventional (dict): Dictionary of the form {node_name: value} specifying the intervention values for the nodes.
-            Can either be a single value or a numpy array of values, with the same length as the number of samples.
-        verbose (bool) : if true, print the progress of the generation
+            - n_samples (int) : The length of the generated dataset  
+            - warmup_steps (int) : Number of excess forward steps to perform at start, and then discard them; this is done as 
+            the initialization is peformed through pure noise, which is not representative of the time-series and degraded the 
+            quality of the casual; TODO: determine through plots, statistical measures etc a reasonable default warmup-step
+            - with_effect_size (bool) : Default is `False`. If `True`, multiplies each fetched parent values with 
+                        its corresponding effect size 
+            - clipping (bool) : if `True`, clips the values to a specific range
+            - verbose (bool) : if `True`, pring the progress of the generation
 
-        Return
-        ------
-        time_series (pandas.DataFrame) :the time-series data
+        Returns
+        ----
+            - time_series (pandas.DataFrame) : The time-series data
         """
         for _ in trange(warmup_steps + n_samples):
-
-            self.forward(with_effect_size=with_effect_size, clipping=clipping, interventional=interventional, 
-                         intervention_type=intervention_type, verbose=verbose)
+            self.forward(with_effect_size=with_effect_size, clipping=clipping, verbose=verbose)
         
         self.time_series = self.time_series.loc[self.causal_structure.n_lags + warmup_steps :, :].reset_index(drop=True)
         return self.time_series  
